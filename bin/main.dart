94f9de5d7140b6c55f1d5358f17b93e70cb9f99e
Bryan Cairns <bcairns@voidrealms.com>
2021-07-10 15:15:08 -0400

    Update main.dart

--- bin/main.dart
+++ bin/main.dart
@@ -4,7 +4,8 @@ main(List<String> arguments) {
 
   var url = "http://www.voidrealms.com";
 
-  http.get(url).then((response) {
+ // http.get(url).then((response) {
+  http.get(Uri.parse(site)).then((response) {
     print("Response status code: ${response.statusCode}"); //200 means OK
     print("Response body: ${response.body}");
   });
